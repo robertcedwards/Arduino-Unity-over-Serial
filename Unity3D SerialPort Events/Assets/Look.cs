c9eec30778d757be391043c23c57440517925fb3
Robert C Edwards <Github@coactlab.com>
2015-09-16 08:41:11 -0700

    init files

---
+++ Unity3D SerialPort Events/Assets/Look.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Look : MonoBehaviour {
+	void Update() {
+		//RaycastHit hit;
+		//UnitySerialPort unitySerialPort = new UnitySerialPort();
+		//float distanceToGround = 0;
+		
+		//	if (Physics.Raycast(transform.position, -Vector3.forward, out hit, 100.0F)) {
+			//distanceToGround = hit.distance;
+		//	print(distanceToGround);
+			//unitySerialPort.SerialPort.WriteLine("A" + distanceToGround);
+
+		//	}
+		}
+
+}
---
+++ Unity3D SerialPort Events/Assets/flashlight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flashlight : MonoBehaviour {
+
+	public bool on = false;
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.F))
+			on = !on;
+		if(on)
+			GetComponent<Light>().enabled = true;
+		else if(!on)
+			GetComponent<Light>().enabled = false;
+	}
+}
